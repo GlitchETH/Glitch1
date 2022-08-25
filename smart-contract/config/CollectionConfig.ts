6232b9b0a5d8fcbace6099e6101d807bd5f70ee3
GlitchETH <103063172+GlitchETH@users.noreply.github.com>
2022-08-25 16:30:36 -0400

    build

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,10 +9,10 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  contractName: 'Glitch',
+  tokenName: 'Glitch',
+  tokenSymbol: 'GLC',
+  hiddenMetadataUri: 'ipfs://QmXWMR2jEeB99VtMgXdYV6BgcYXKyD4mWpne1eVpKQBQkf/Hidden.json',
   maxSupply: 10000,
   whitelistSale: {
     price: 0.05,
@@ -26,7 +26,7 @@ const CollectionConfig: CollectionConfigInterface = {
     price: 0.09,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  contractAddress: '0x95941c22b3463fE03A4Bef51D71d7359D2c0F7Ec',
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
